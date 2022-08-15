eb76c4d4ccf2765fe6d5fe6049d0258694230fe8
sonuengineer <prajapatisonu50@gmail.com>
2022-08-15 16:39:42 +0530

    filter mach

--- main.js
+++ main.js
@@ -48,3 +48,20 @@ function removeItem(e){
     }
   }
 }
+
+
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
